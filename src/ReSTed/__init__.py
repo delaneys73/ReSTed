7ad59dc024f91530097546f7c65707663c4c9188
Stephen Delaney <delaneys73@gmail.com>
2011-07-13 08:31:58 +1000

    Added ability to edit in raw mode

--- src/ReSTed/__init__.py
+++ src/ReSTed/__init__.py
@@ -7,6 +7,7 @@ import glob
 from docutils import core, io
 import subprocess
 import html2rst
+import webbrowser
 
 config = {}
 mainTemplate = ""
@@ -22,16 +23,19 @@ def setup(baseDir,webRoot):
     ReSTed.mainTemplate = "%(templateDir)smain.%(templateExt)s" % ReSTed.config
 
 def getRST(html):
-    if config["usePandoc"]==True:
+    if ReSTed.config["usePandoc"]==True:
         p = subprocess.Popen(['pandoc', '--from=html', '--to=rst'],
                          stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         return p.communicate(html)[0]
     else:
         return html2rst.html2text(html)
+
+def getTemplatePath(view):
+    return "%(templateDir)s%(view)s.%(templateExt)s" % dict(ReSTed.config,**{"view":view})
     
 def getTemplate(view,title):
         indexTpl = Template(file=ReSTed.mainTemplate) 
-        indexTpl.body = "%(templateDir)s%(view)s.%(templateExt)s" % dict(ReSTed.config,**{"view":view})
+        indexTpl.body = getTemplatePath(view)
         indexTpl.title = title
         indexTpl.templateDir = ReSTed.config["templateDir"]
         return indexTpl
@@ -39,17 +43,18 @@ def getTemplate(view,title):
 class ReSTed:
     
     def index(self):
-        return self._browse(ReSTed.config["baseDir"],False)
+        return self._browse(ReSTed.config["baseDir"],"",False)
     index.exposed = True
 
     def browse(self,dir):
-        return self._browse(ReSTed.config["baseDir"]+"/"+dir,True)
+        return self._browse(ReSTed.config["baseDir"]+"/"+dir,dir+"/",True)
     browse.exposed = True
 
-    def _browse(self,dir,showBack):
+    def _browse(self,dir,subdir,showBack):
         tpl = getTemplate("index","")
         tpl.showBack = showBack
         tpl.files = glob.glob1(dir, "*.rst")
+        tpl.subdir = subdir
         tpl.images = glob.glob1(dir, "*.png") + glob.glob1(dir, "*.jpg")
         dirs = [ name for name in os.listdir(dir) if os.path.isdir(os.path.join(dir, name)) ]
         tpl.directories = dirs
@@ -58,28 +63,53 @@ class ReSTed:
     def edit(self,file):
         return self._edit(file,"")
 
+    def _getEditor(self):
+        cookieEditor = "wysiwyg"
+        if cherrypy.request.cookie.has_key("rested.editor"):
+            morsel = cherrypy.request.cookie["rested.editor"]
+            cookieEditor = morsel.value
+        return cookieEditor
+    
     def _edit(self,file,message):
+        rawedit = False
+        
+        cookieEditor = self._getEditor(); 
+        
+        if cookieEditor=="raw":
+            rawedit = True
+        
         tpl= getTemplate("edit",file)
         tpl.message = message
         tpl.file = file
-        fileName = ReSTed.config["baseDir"]+"/"+file
+        fileName = "%s/%s" % (ReSTed.config["baseDir"],file)
         fp = open(fileName, 'r')
         rst = fp.read()
         tpl.rst = rst
-        overrides = {'doctitle_xform': file,
+        
+        if rawedit:
+            tpl.editor = getTemplatePath("editraw")
+        else:
+            tpl.editor = getTemplatePath("editgui")
+            overrides = {'doctitle_xform': file,
                  'initial_header_level': 1}
-        parts = core.publish_parts(
-        source=rst, source_path=ReSTed.config["baseDir"],
-        destination_path=ReSTed.config["baseDir"],
-        writer_name='html', settings_overrides=overrides)
-        fragment = parts['html_body']
-        tpl.html = fragment
+        
+            parts = core.publish_parts(
+                source=rst, source_path=ReSTed.config["baseDir"],
+                destination_path=ReSTed.config["baseDir"],
+                writer_name='html', settings_overrides=overrides)
+            
+            fragment = parts['html_body']
+            tpl.html = fragment
         
         return str(tpl)
     edit.exposed = True
 
-    def save(self,file,html):
-        rstsrc = getRST(html)
+    def save(self,file,html,rst,source):
+        if source=="html":
+            rstsrc = getRST(html)
+        else:
+            rstsrc = rst
+            
         fileName = ReSTed.config["baseDir"]+"/"+file
         fp = open(fileName,'w')
         fp.write(rstsrc)
@@ -90,14 +120,30 @@ class ReSTed:
     def shutdown(self):
         cherrypy.engine.exit()
     shutdown.exposed = True
-
+    
+    def preview(self,rst,file):
+        tpl= Template(file=getTemplatePath("preview"))
+        
+        overrides = {'doctitle_xform': file,
+             'initial_header_level': 1}
+    
+        parts = core.publish_parts(
+            source=rst, source_path=ReSTed.config["baseDir"],
+            destination_path=ReSTed.config["baseDir"],
+            writer_name='html', settings_overrides=overrides)
+        
+        fragment = parts['html_body']
+        tpl.html = fragment
+        return str(tpl)
+    preview.exposed = True
 
 def start(baseDir,webRoot):
     setup(baseDir, webRoot)
+    port = 8000
     webconfig = {
         'global': 
         {
-            'server.socket_port': 8000,
+            'server.socket_port': port,
             'server.thread_pool': 10
         },
         '/include':
@@ -111,4 +157,7 @@ def start(baseDir,webRoot):
             'tools.staticdir.dir':baseDir
         }
     }
+    webbrowser.open_new("http://127.0.0.1:%s" % port)
     cherrypy.quickstart(ReSTed(),"/",webconfig)
+    
+    
